a9c35020de2fbda9ed6b77a78e86795e7ac966bf
blaz2f <46502527+blaz2f@users.noreply.github.com>
2024-09-12 18:44:38 +1000

    updating build file

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,17 +1,4 @@
-#
-# Builds a Docker image.
-#
-# Environment variables:
-#
-#   CONTAINER_REGISTRY - The hostname of your container registry.
-#   VERSION - The version number to tag the images with.
-#   NAME - The name of the image to build.
-#   DIRECTORY - The directory form which to build the image.
-#
-# Usage:
-#
-#       ./scripts/cd/build-image.sh
-#
+#!/bin/bash
 
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
